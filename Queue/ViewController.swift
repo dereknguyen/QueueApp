24d695fee300aae41ec7e5ed164fbcef7c55109a
Derek Nguyen <derekn.dev@gmail.com>
2019-10-22 10:56:31 -0700

    Initial Commit

---
+++ Queue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Queue
+//
+//  Created by Derek Nguyen on 10/22/19.
+//  Copyright © 2019 Derek Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
